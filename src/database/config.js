d352c60c07fa94e6a4dcb400894f3e9118965d67
fcr3 <fcreyes@berkeley.edu>
2018-12-06 16:40:05 -0800

    added pictures of pages

---
+++ src/database/config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCxyUNLviHiI_9kAWLTreSpInPlQthxq5Q",
+  authDomain: "bab-design-website.firebaseapp.com",
+  databaseURL: "https://bab-design-website.firebaseio.com",
+  projectId: "bab-design-website",
+  storageBucket: "bab-design-website.appspot.com",
+  messagingSenderId: "781111193996"
+};
+firebase.initializeApp(config);
